0eb664c50e66b592e29595652921a6c834c2e8e4
Michael Greenberg <michael.greenberg@stevens.edu>
2022-01-20 18:30:57 -0500

    basic parser working

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
+#[macro_use]
+extern crate lalrpop_util;
+
+use std::io::Read;
+
+use log::error;
+
+mod syntax;
+use crate::syntax::*;
+
 fn main() {
-    println!("Hello, world!");
+    let config = clap::App::new(env!("CARGO_PKG_NAME"))
+        .version(env!("CARGO_PKG_VERSION"))
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .arg(
+            clap::Arg::new("INPUT")
+                .help("Sets the input file (defaults to '-', meaning STDIN)")
+                .default_value("-")
+                .index(1),
+        )
+        .get_matches();
+
+    let input_source = config.value_of("INPUT").expect("input source");
+    let mut input = String::new();
+    let res = if input_source == "-" {
+        std::io::stdin().read_to_string(&mut input)
+    } else {
+        std::fs::File::open(input_source).and_then(|mut f| f.read_to_string(&mut input))
+    };
+
+    if let Err(err) = res {
+        error!("I/O error: {}", err);
+        std::process::exit(47);
+    }
+
+    let p = Program::parse(&input).unwrap_or_else(|e| {
+        error!("Parse error:\n{}", e);
+        std::process::exit(2);
+    });
+
+    for c in p.0 {
+        println!("{:?}", c);
+    }
 }
---
+++ src/syntax.rs
@@ -0,0 +1,54 @@
+//use std::fmt::Display;
+
+lalrpop_mod!(parser);
+
+#[derive(Debug)]
+pub struct Program(pub Vec<Constraint>);
+
+#[derive(Debug)]
+pub enum Constraint {
+    Rule(Head, Vec<Literal>),
+    Fact(Head),
+    Integrity(Vec<Literal>),
+}
+
+#[derive(Debug)]
+pub enum Literal {
+    Atom(Atom),
+    Not(Atom),
+}
+
+#[derive(Debug)]
+pub enum Head {
+    Simple(Symbol),
+    Fun(Variable, Vec<SimpleTerm>)
+}
+
+#[derive(Debug)]
+pub enum Atom {
+    Simple(SimpleTerm),
+    Fun(Variable, Vec<SimpleTerm>),
+}
+
+#[derive(Debug)]
+pub enum SimpleTerm {
+    Symbol(Symbol),
+    Variable(Variable),
+    FreshVar(),
+    Int(isize),
+    Supremum,
+    Infimum,
+}
+
+pub type Symbol = String;
+pub type Variable = String;
+
+impl Program {
+    pub fn parse(s: &str) -> Result<Self, String> {
+        parser::ProgramParser::new()
+            .parse(s)
+            .map_err(|e| e.to_string())
+    }
+}
+
+// TODO: pretty
